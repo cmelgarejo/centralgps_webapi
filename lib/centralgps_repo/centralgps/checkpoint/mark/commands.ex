1836e340777a959b1435c73ec1105efcb12decf5
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-05-29 20:25:01 -0400

    trying to understand where the variables should be offset and limit are messing with me! :(

--- lib/centralgps_repo/centralgps/checkpoint/mark/commands.ex
+++ lib/centralgps_repo/centralgps/checkpoint/mark/commands.ex
@@ -1,4 +1,4 @@
 defmodule CentralGPS.Repo.Checkpoint.Mark.Commands do
   def cmd_fn_api_monitor_mark_list,
-    do: "SELECT * FROM checkpoint.fn_api_monitor_mark_list($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8::timestamp, $9::timestamp);"
+    do: "SELECT * FROM checkpoint.fn_api_monitor_mark_list($1, $2, $3, $4, $5::jsonb, $8::timestamp, $9::timestamp, $6::bigint, $7::bigint);"
 end
