7d36ae78d583aec65a537361bc1c1d3788def91a
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-05-29 20:09:55 -0400

    changed the order of parameters in db fn calling :()

--- lib/centralgps_repo/centralgps/checkpoint/mark/commands.ex
+++ lib/centralgps_repo/centralgps/checkpoint/mark/commands.ex
@@ -1,4 +1,4 @@
 defmodule CentralGPS.Repo.Checkpoint.Mark.Commands do
   def cmd_fn_api_monitor_mark_list,
-    do: "SELECT * FROM client.fn_api_monitor_mark_list($1, $2, $3, $4, $5::jsonb, $6::timestamp, $7::timestamp, $8::bigint, $9::bigint);"
+    do: "SELECT * FROM client.fn_api_monitor_mark_list($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8::timestamp, $9::timestamp);"
 end
