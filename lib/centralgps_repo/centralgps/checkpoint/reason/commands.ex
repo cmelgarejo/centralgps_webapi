afa7420d7c0cee36af48422bd9f91bfc1b3f642b
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-06-17 21:10:56 -0400

    Reason commands fixed

--- lib/centralgps_repo/centralgps/checkpoint/reason/commands.ex
+++ lib/centralgps_repo/centralgps/checkpoint/reason/commands.ex
@@ -2,7 +2,7 @@ defmodule CentralGPS.Repo.Checkpoint.Reason.Commands do
   #DB Definition: checkpoint.fn_api_reason_create (_auth_token text, _auth_type, _the_app_name, _the_ip_port, _xtra_info, reason_configuration_id, reason_description) RETURNS common.return_type_generic
   #Input JSON: [ :configuration_id, :description ]
   def cmd_fn_api_reason_create,
-    do: "SELECT * FROM checkpoint.fn_api_reason_create($1, $2, $3, $4, $5::jsonb, $6::bigint, $7);"
+    do: "SELECT * FROM checkpoint.fn_api_reason_create($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8);"
 
   #DB Definition: checkpoint.fn_api_reason_read (_auth_token text, _auth_type, _the_app_name, _the_ip_port, _xtra_info, reason_id) RETURNS common.return_type_generic
   #Input JSON:[ :reason_id ]
@@ -12,7 +12,7 @@ defmodule CentralGPS.Repo.Checkpoint.Reason.Commands do
   #DB Definition: checkpoint.fn_api_reason_create (_auth_token text, _auth_type, _the_app_name, _the_ip_port, _xtra_info, reason_id, reason_description) RETURNS common.return_type_generic
   #Input JSON: [ :reason_id ]
   def cmd_fn_api_reason_update,
-    do: "SELECT * FROM checkpoint.fn_api_reason_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7);"
+    do: "SELECT * FROM checkpoint.fn_api_reason_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8);"
 
   #DB Definition: checkpoint.fn_api_reason_delete (_auth_token text, _my_auth_type, _the_app_name text, _the_ip_port text, _xtra_info, reason_id) RETURNS common.return_type_generic
   #Input JSON: [ :reason_id, :reason_description ]
