e3aed00bfb8227d7c61df740cf832897a538ae4f
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-06-20 22:56:18 -0400

    Modding the sendfile schema

--- web/controllers/checkpoint/admin/venue_type.ex
+++ web/controllers/checkpoint/admin/venue_type.ex
@@ -6,7 +6,7 @@ defmodule CentralGPSWebAPI.Controllers.Checkpoint.VenueType do
 
   def create(conn, _params) do
     try do
-      _k = [ :configuration_id, :description, :image ]
+      _k = [ :configuration_id, :description, :image, :image_file ]
       {headers, _params} = auth_proc_headers_and__params(conn.req_headers, _params, _k)
       {row_count, result} = _params
         |> (Map.update :reason_configuration_id, 0, fn(v)->(if !is_integer(v), do: elem(Integer.parse(v), 0), else: v) end)
@@ -14,7 +14,7 @@ defmodule CentralGPSWebAPI.Controllers.Checkpoint.VenueType do
         |> fn_api_venue_type_create
         {response_code, result} = (if result.status, do: {201, result},
                                    else: {200, result |> Map.take [:status, :msg]})
-        if (response_code == 201 && Map.has_key? :file ), do: save_image(_params.image, _params.file)
+        if (response_code == 201 && Map.has_key? :image_file ), do: save_image(_params.image, _params.image_file)
         json (conn |> put_status response_code), result
     rescue
       e in ArgumentError -> json (conn |> put_status 400), %{status: false, msg: e.message}
@@ -46,7 +46,7 @@ defmodule CentralGPSWebAPI.Controllers.Checkpoint.VenueType do
         |> (Map.update :venue_type_id, 0, fn(v)->(if !is_integer(v), do: elem(Integer.parse(v), 0), else: v) end)
       {row_count, result} = fn_api_venue_type_read (Map.drop(_params, _k) |> Map.values) ++ [_params.venue_type_id]
       if result.status do
-        save_image(_params.image, _params.file, result.image)
+        save_image(_params.image, _params.image_file, result.image)
         {row_count, result} = fn_api_venue_type_update((Map.drop(_params, _k) |> Map.values) ++ [_params.venue_type_id, _params.configuration_id,
           _params.description, _params.image])
       end
@@ -55,15 +55,6 @@ defmodule CentralGPSWebAPI.Controllers.Checkpoint.VenueType do
       e in ArgumentError -> json (conn |> put_status 400), %{status: false, msg: e.message}
       e in Exception -> json (conn |> put_status 500), %{status: false, msg: e.message}
     end
-  end
-
-  defp _local_image_path, do: Endpoint.config(:root) <> "priv/static"
-  defp dest_dir(filename), do: String.split(filename, "/") |> Enum.reverse |> tl |> Enum.reverse |> Enum.join
-  defp save_image(filename, file, old_file \\ "") do
-    if (old_file != ""), do: File.rm Enum.join([ _local_image_path,  old_file ], "/") #removes the old image
-    if (File.exists?dest_dir(filename)), do: File.mkdir_p dest_dir(filename)
-    filename = Enum.join [ _local_image_path, dest_dir(filename), filename ], "/"
-    File.write filename, base64_decode(file)
   end
 
   def delete(conn, _params) do
@@ -93,4 +84,16 @@ defmodule CentralGPSWebAPI.Controllers.Checkpoint.VenueType do
       e in Exception -> json (conn |> put_status 500), %{status: false, msg: e.message}
     end
   end
+
+  defp _local_image_path, do: Endpoint.config(:root) <> "priv/static"
+  defp dest_dir(filename), do: String.split(filename, "/") |> Enum.reverse |> tl |> Enum.reverse |> Enum.join
+  defp save_image(filename, file, old_filename \\ "") do
+    if !is_nil(filename) do
+      if (old_filename != ""), do: File.rm Enum.join([ _local_image_path,  old_filename ], "/") #removes the old image
+      if (File.exists?dest_dir(filename)), do: File.mkdir_p dest_dir(filename)
+      filename = Enum.join [ _local_image_path, dest_dir(filename), filename ], "/"
+      File.write filename, base64_decode(file)
+    end
+  end
+  
 end
