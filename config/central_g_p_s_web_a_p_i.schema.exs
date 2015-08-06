1c3581b299c28ddb7584a1871a03754ada7a89a1
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-08-06 11:40:00 -0400

    Added conform configuration files

---
+++ config/central_g_p_s_web_a_p_i.schema.exs
@@ -0,0 +1,174 @@
+[
+  mappings: [
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.url.host": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.url.host here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.url.host",
+      datatype: :binary,
+      default: "api.centralgps.net"
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.root": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.root here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.root",
+      datatype: :binary,
+      default: "c:/CM/Projects/CentralGPS/server/api/centralgps_webapi"
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.secret_key_base": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.secret_key_base here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.secret_key_base",
+      datatype: :binary,
+      default: "aLIFKvszPTOoXoAYQ3RL9qdUfmhEyEesWFMi5OwQd80sME26l3tyrZzb9digCZXj"
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.debug_errors": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.debug_errors here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.debug_errors",
+      datatype: :atom,
+      default: true
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.code_reloader": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.code_reloader here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.code_reloader",
+      datatype: :atom,
+      default: true
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.server": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.server here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.server",
+      datatype: :atom,
+      default: true
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.http.port": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.http.port here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.http.port",
+      datatype: :integer,
+      default: 4000
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.cache_static_lookup": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.cache_static_lookup here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPSWebAPI.Endpoint.cache_static_lookup",
+      datatype: :atom,
+      default: false
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.adapter": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.adapter here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.adapter",
+      datatype: :atom,
+      default: Ecto.Adapters.Postgres
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.extensions.Elixir.CentralGPS.Repo.Extensions.Postgrex.JSON.library": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.extensions.Elixir.CentralGPS.Repo.Extensions.Postgrex.JSON.library here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.extensions.Elixir.CentralGPS.Repo.Extensions.Postgrex.JSON.library",
+      datatype: :atom,
+      default: Poison
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.username": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.username here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.username",
+      datatype: :binary,
+      default: "postgres"
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.password": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.password here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.password",
+      datatype: :binary,
+      default: "!pgsql@94#"
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.database": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.database here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.database",
+      datatype: :binary,
+      default: "centralgps"
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.hostname": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.hostname here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.hostname",
+      datatype: :binary,
+      default: "184.171.247.181"
+    ],
+    "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.port": [
+      doc: "Provide documentation for central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.port here.",
+      to: "central_g_p_s_web_a_p_i.Elixir.CentralGPS.Repo.port",
+      datatype: :integer,
+      default: 6543
+    ],
+    "app_config.checkpoint_enabled": [
+      doc: "Provide documentation for app_config.checkpoint_enabled here.",
+      to: "app_config.checkpoint_enabled",
+      datatype: :atom,
+      default: true
+    ],
+    "logger.utc_log": [
+      doc: "Provide documentation for logger.utc_log here.",
+      to: "logger.utc_log",
+      datatype: :atom,
+      default: true
+    ],
+    "logger.console.format": [
+      doc: "Provide documentation for logger.console.format here.",
+      to: "logger.console.format",
+      datatype: :binary,
+      default: """
+      $date $time [$level] $metadata - $message
+      """
+    ],
+    "logger.console.metadata": [
+      doc: "Provide documentation for logger.console.metadata here.",
+      to: "logger.console.metadata",
+      datatype: [
+        list: :atom
+      ],
+      default: [
+        :request_id
+      ]
+    ],
+    "logger.debug_log.path": [
+      doc: "Provide documentation for logger.debug_log.path here.",
+      to: "logger.debug_log.path",
+      datatype: :binary,
+      default: "logs/debug.log"
+    ],
+    "logger.debug_log.level": [
+      doc: "Provide documentation for logger.debug_log.level here.",
+      to: "logger.debug_log.level",
+      datatype: :atom,
+      default: :debug
+    ],
+    "logger.error_log.path": [
+      doc: "Provide documentation for logger.error_log.path here.",
+      to: "logger.error_log.path",
+      datatype: :binary,
+      default: "logs/error.log"
+    ],
+    "logger.error_log.level": [
+      doc: "Provide documentation for logger.error_log.level here.",
+      to: "logger.error_log.level",
+      datatype: :atom,
+      default: :error
+    ],
+    "logger.info_log.path": [
+      doc: "Provide documentation for logger.info_log.path here.",
+      to: "logger.info_log.path",
+      datatype: :binary,
+      default: "logs/info.log"
+    ],
+    "logger.info_log.level": [
+      doc: "Provide documentation for logger.info_log.level here.",
+      to: "logger.info_log.level",
+      datatype: :atom,
+      default: :info
+    ],
+    "logger.warn_log.path": [
+      doc: "Provide documentation for logger.warn_log.path here.",
+      to: "logger.warn_log.path",
+      datatype: :binary,
+      default: "logs/warn.log"
+    ],
+    "logger.warn_log.level": [
+      doc: "Provide documentation for logger.warn_log.level here.",
+      to: "logger.warn_log.level",
+      datatype: :atom,
+      default: :warn
+    ]
+  ],
+  translations: [
+  ]
+]
