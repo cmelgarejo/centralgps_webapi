7f19106a805451c80c0306c13f65b3d046883ddd
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-09-17 18:06:39 -0400

    changes venue_type To reflect image_path and _bin usage

--- lib/centralgps_repo/centralgps/checkpoint/venue_type/commands.ex
+++ lib/centralgps_repo/centralgps/checkpoint/venue_type/commands.ex
@@ -2,7 +2,7 @@ defmodule CentralGPS.Repo.Checkpoint.VenueType.Commands do
   #DB Definition: checkpoint.fn_api_venue_type_create (_auth_token text, _auth_type, _the_app_name, _the_ip_port, _xtra_info, venue_type_configuration_id, venue_type_description, venue_type_image) RETURNS common.return_type_generic
   #Input JSON: [ :configuration_id, :description, :image ]
   def cmd_fn_api_venue_type_create,
-    do: "SELECT * FROM checkpoint.fn_api_venue_type_create($1, $2, $3, $4, $5::jsonb, $6::bigint, $7, $8);"
+    do: "SELECT * FROM checkpoint.fn_api_venue_type_create($1, $2, $3, $4, $5::jsonb, $6::bigint, $7, $8, $9::bytea);"
 
   #DB Definition: checkpoint.fn_api_venue_type_read (_auth_token text, _auth_type, _the_app_name, _the_ip_port, _xtra_info, venue_type_id) RETURNS common.return_type_generic
   #Input JSON:[ "venue_type_id", "account_type"]
@@ -12,7 +12,7 @@ defmodule CentralGPS.Repo.Checkpoint.VenueType.Commands do
   #DB Definition: checkpoint.fn_api_venue_type_create (_auth_token text, _auth_type, _the_app_name, _the_ip_port, _xtra_info, venue_type_configuration_id, venue_type_description, venue_type_id, venue_type_image) RETURNS common.return_type_generic
   #Input JSON: [ :venue_type_id, :configuration_id, :description, :image]
   def cmd_fn_api_venue_type_update,
-    do: "SELECT * FROM checkpoint.fn_api_venue_type_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8, $9);"
+    do: "SELECT * FROM checkpoint.fn_api_venue_type_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8, $9, $10::bytea);"
 
   #DB Definition: checkpoint.fn_api_venue_type_delete (_auth_token text, _auth_type, _the_app_name text, _the_ip_port text, _xtra_info, venue_type_id) RETURNS common.return_type_generic
   #Input JSON: [ "venue_type_id" ]
