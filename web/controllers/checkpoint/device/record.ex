b0a3eddee0b27a643bd064cd8ad2e1ee7a60e30c
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-10-04 10:12:43 -0300

    Testing record method

--- web/controllers/checkpoint/device/record.ex
+++ web/controllers/checkpoint/device/record.ex
@@ -5,21 +5,20 @@ defmodule CentralGPSWebAPI.Controllers.Device.Record do
 
   def record(conn, params) do
     try do
-      keys = [ :accuracy, :form_id, :address, :altitude,
-        :bearing, :comment, :lat, :lon, :mark, :position_at,
-        :activity_id, :speed, :status, :xtra_info ]
+      keys = [ :lat, :lon, :speed, :bearing, :accuracy, :altitude, :status, :address, :position_at ]
       {headers, params} = checkpoint_auth_proc_headers_and_params(conn.req_headers, params, keys)
       ip_header = :"x-forwarded-for"
       ip = if Map.has_key?(headers,ip_header), do: to_string(headers[ip_header]), else: nil
-      {_, result} = params
-        |>  Map.update(:position_at,  nil, &(parse_datetime(&1)))
-        |>  Map.update(:lat,          nil, &(parse_float(&1)))
+      params = params
+        |>  Map.update(:accuracy,     nil, &(parse_float(&1)))
+        |>  Map.update(:altitude,     nil, &(parse_float(&1)))
+        |>  Map.update(:lat,          nil, &((&1)))
         |>  Map.update(:lon,          nil, &(parse_float(&1)))
-        |>  Map.update(:accuracy,     nil, &(parse_datetime(&1)))
-        |>  Map.update(:altitude,     nil, &(parse_datetime(&1)))
-        |> (Map.put :_ip_port, ip)
-        |> Map.values #|> Enum.concat([nil]) #add xtra_info
-        |> fn_chkapi_record
+        |>  Map.update(:position_at,  nil, &(parse_datetime(&1)))
+        |> (Map.put :ip_port, ip)
+      {_, result} = fn_chkapi_record ([ params.lat, params.lon, params.speed,
+        params.bearing, params.accuracy, params.altitude, params.status,
+        params.address, params.ip_port, params.position_at])
       response_code = if result.status, do: 201, else: 200
       json (conn |> put_status response_code), result
     rescue
