c1f2333fcc1498eb2bd619e25a0f08e1d1f5adc4
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-10-23 12:03:06 -0300

    fixed ITEM commands parameters

--- lib/centralgps_repo/centralgps/checkpoint/item/commands.ex
+++ lib/centralgps_repo/centralgps/checkpoint/item/commands.ex
@@ -2,7 +2,7 @@ defmodule CentralGPS.Repo.Checkpoint.Item.Commands do
   #DB Definition: checkpoint.fn_api_item_create (_auth_token, _auth_type, _the_app_name, _the_ip_port, _xtra_info, item_configuration_id, item_name, item_description, item_notes, item_stock) RETURNS common.return_type_generic
   #Input JSON: [ item_configuration_id, item_name, item_description, item_xtra_info ]
   def cmd_fn_api_item_create,
-    do: "SELECT * FROM checkpoint.fn_api_item_create($1, $2, $3, $4, $5::jsonb, $6::bigint, $7, $8, $9::jsonb);"
+    do: "SELECT * FROM checkpoint.fn_api_item_create($1, $2, $3, $4, $5::jsonb, $6::bigint, $7, $8, $9, $10::double precision, $11::double precision, $12::double precision);"
 
   #DB Definition: checkpoint.fn_api_item_read (_auth_token, _auth_type, _the_app_name, _the_ip_port, _xtra_info, item_id) RETURNS common.return_type_generic
   #Input JSON:[ :item_id ]
@@ -12,7 +12,7 @@ defmodule CentralGPS.Repo.Checkpoint.Item.Commands do
   #DB Definition: checkpoint.fn_api_item_update (_auth_token, _auth_type, _the_app_name, _the_ip_port, _xtra_info, item_configuration_id, item_name, item_description, item_notes, item_stock) RETURNS common.return_type_generic
   #Input JSON: [ :item_id, item_configuration_id, item_name, item_description, item_xtra_info ]
   def cmd_fn_api_item_update,
-    do: "SELECT * FROM checkpoint.fn_api_item_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8::bigint, $9, $10, $11::jsonb);"
+    do: "SELECT * FROM checkpoint.fn_api_item_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8, $9, $10, $11::double precision, $12::double precision, $13::double precision);"
 
   #DB Definition: checkpoint.fn_api_item_delete (_auth_token, _my_auth_type, _the_app_name, _the_ip_port, _xtra_info, item_id) RETURNS common.return_type_generic
   #Input JSON: [ :item_id ]
