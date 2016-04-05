cc3e7cfd34ac4a6ef5629265ad7574b7b9b41c4c
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2016-04-05 17:38:52 -0400

    Added xtra_info column to checkpoint.item

--- lib/centralgps_repo/centralgps/checkpoint/item/commands.ex
+++ lib/centralgps_repo/centralgps/checkpoint/item/commands.ex
@@ -2,7 +2,7 @@ defmodule CentralGPS.Repo.Checkpoint.Item.Commands do
   #DB Definition: checkpoint.fn_api_item_create (_auth_token, _auth_type, _the_app_name, _the_ip_port, _xtra_info, item_configuration_id, item_name, item_description, item_notes, item_stock) RETURNS common.return_type_generic
   #Input JSON: [ item_configuration_id, item_name, item_description, item_xtra_info ]
   def cmd_fn_api_item_create,
-    do: "SELECT * FROM checkpoint.fn_api_item_create($1, $2, $3, $4, $5::jsonb, $6::bigint, $7, $8, $9, $10::double precision, $11::double precision, $12::double precision);"
+    do: "SELECT * FROM checkpoint.fn_api_item_create($1, $2, $3, $4, $5::jsonb, $6::bigint, $7, $8, $9, $10::double precision, $11::double precision, $12::double precision, $13::jsonb);"
 
   #DB Definition: checkpoint.fn_api_item_read (_auth_token, _auth_type, _the_app_name, _the_ip_port, _xtra_info, item_id) RETURNS common.return_type_generic
   #Input JSON:[ :item_id ]
@@ -12,7 +12,7 @@ defmodule CentralGPS.Repo.Checkpoint.Item.Commands do
   #DB Definition: checkpoint.fn_api_item_update (_auth_token, _auth_type, _the_app_name, _the_ip_port, _xtra_info, item_configuration_id, item_name, item_description, item_notes, item_stock) RETURNS common.return_type_generic
   #Input JSON: [ :item_id, item_configuration_id, item_name, item_description, item_xtra_info ]
   def cmd_fn_api_item_update,
-    do: "SELECT * FROM checkpoint.fn_api_item_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8, $9, $10, $11::double precision, $12::double precision, $13::double precision);"
+    do: "SELECT * FROM checkpoint.fn_api_item_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8, $9, $10, $11::double precision, $12::double precision, $13::double precision, $14::jsonb);"
 
   #DB Definition: checkpoint.fn_api_item_delete (_auth_token, _my_auth_type, _the_app_name, _the_ip_port, _xtra_info, item_id) RETURNS common.return_type_generic
   #Input JSON: [ :item_id ]
--- web/controllers/checkpoint/admin/item.ex
+++ web/controllers/checkpoint/admin/item.ex
@@ -5,14 +5,15 @@ defmodule CentralGPSWebAPI.Controllers.Checkpoint.Item do
 
   def create(conn, params) do
     try do
-      keys = [ :configuration_id, :name, :description, :notes, :stock, :min_qty, :max_qty ]
+      keys = [ :configuration_id, :name, :description, :notes, :stock, :min_qty, :max_qty, :xtra_info ]
       {_, params} = auth_proc_headers_and_params(conn.req_headers, params, keys)
       params = params
         |> Map.update(:configuration_id, nil, &(parse_int(&1)))
         |> Map.update(:min_qty, nil, &(parse_float(&1)))
         |> Map.update(:max_qty, nil, &(parse_float(&1)))
       {_, result} = fn_api_item_create((Map.drop(params, keys) |> Map.values) ++
-        [ params.configuration_id, params.name, params.description, params.notes, params.stock, params.min_qty, params.max_qty ])
+        [ params.configuration_id, params.name, params.description, params.notes,
+          params.stock, params.min_qty, params.max_qty, params.xtra_info ])
       {response_code, result} = (if result.status, do: {201, result},
                                  else: {200, result |> Map.take([:status, :msg])})
       json((conn |> put_status(response_code)), result)
@@ -39,7 +40,7 @@ defmodule CentralGPSWebAPI.Controllers.Checkpoint.Item do
 
   def update(conn, params) do
     try do
-      keys = [ :item_id, :configuration_id, :name, :description, :notes, :stock, :min_qty, :max_qty ]
+      keys = [ :item_id, :configuration_id, :name, :description, :notes, :stock, :min_qty, :max_qty, :xtra_info ]
       {_, params} = auth_proc_headers_and_params(conn.req_headers, params, keys)
       params = params
         |> Map.update(:item_id, nil, &(parse_int(&1)))
@@ -50,7 +51,8 @@ defmodule CentralGPSWebAPI.Controllers.Checkpoint.Item do
         [params.item_id] #get the record and check first
       if result.status do
         {_, result} = fn_api_item_update((Map.drop(params, keys) |> Map.values) ++ #drop the params first, and leave only the "head" parameters, auth_token, auth_type, app_name, ip, and notes of the caller
-          [ params.item_id, params.configuration_id, params.name, params.description, params.notes, params.stock, params.min_qty, params.max_qty ])
+          [ params.item_id, params.configuration_id, params.name, params.description,
+            params.notes, params.stock, params.min_qty, params.max_qty, params.xtra_info ])
       end
       json (conn |> put_status(200)), result
     rescue
