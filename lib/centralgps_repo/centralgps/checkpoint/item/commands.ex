65e7fe4561394be37028ec26b7d974873ae38b51
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2016-04-16 22:32:38 -0400

    updated the update function

--- lib/centralgps_repo/centralgps/checkpoint/item/commands.ex
+++ lib/centralgps_repo/centralgps/checkpoint/item/commands.ex
@@ -12,7 +12,7 @@ defmodule CentralGPS.Repo.Checkpoint.Item.Commands do
   #DB Definition: checkpoint.fn_api_item_update (_auth_token, _auth_type, _the_app_name, _the_ip_port, _xtra_info, item_configuration_id, item_name, item_description, item_notes, item_stock) RETURNS common.return_type_generic
   #Input JSON: [ :item_id, item_configuration_id, item_name, item_description, item_xtra_info ]
   def cmd_fn_api_item_update,
-    do: "SELECT * FROM checkpoint.fn_api_item_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8, $9, $10, $11, $12, $13::double precision, $14::double precision, $15::double precision, $16::jsonb);"
+    do: "SELECT * FROM checkpoint.fn_api_item_update($1, $2, $3, $4, $5::jsonb, $6::bigint, $7::bigint, $8, $9, $10, $11, $12::double precision, $13::double precision, $14::double precision, $15::jsonb);"
 
   #DB Definition: checkpoint.fn_api_item_delete (_auth_token, _my_auth_type, _the_app_name, _the_ip_port, _xtra_info, item_id) RETURNS common.return_type_generic
   #Input JSON: [ :item_id ]
